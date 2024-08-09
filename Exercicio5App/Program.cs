72bd3cd35ae6f3b0fbe53740b81c58d20165731a
Lucas Ribeiro De Lacerda <32784630803@info.local>
2024-08-09 10:49:32 -0300

    Exercicios Logica de Programação

--- Exercicio5App/Program.cs
+++ Exercicio5App/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Exercício 5
+//Crie um programa que peça ao usuário para digitar um texto e conte quantas vezes cada
+//letra do alfabeto aparece no texto.
+
+
+Dictionary<char, int> letraContagem = new Dictionary<char, int>();
+
+Console.WriteLine("Digite um texto:");
+string texto = Console.ReadLine();
+
+texto = texto.ToLower();
+
+foreach (char c in texto)
+{
+    if (char.IsLetter(c))
+    {
+        if (letraContagem.ContainsKey(c))
+        {
+            letraContagem[c]++;
+        }
+        else
+        {
+            letraContagem[c] = 1;
+        }
+    }
+}
+
+Console.WriteLine("\nContagem das letras:");
+foreach (var item in letraContagem)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
+
