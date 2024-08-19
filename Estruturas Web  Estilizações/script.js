4af6cc81ba9667c75aa8e04e1124a4920ae54899
Lucas Ribeiro De Lacerda <32784630803@info.local>
2024-08-19 09:16:56 -0300

    web Site

--- Estruturas Web  Estilizações/script.js
+++ Estruturas Web  Estilizações/script.js
@@ -1,15 +1,32 @@
-document.getElementById("searchBtn").addEventListener("click", function() {
-    const local = document.getElementById("local").value;
-    const checkin = document.getElementById("checkin").value;
-    const checkout = document.getElementById("checkout").value;
+document.getElementById("searchBtn").addEventListener("click", function () {
+  const local = document.getElementById("local").value;
+  const checkin = document.getElementById("checkin").value;
+  const checkout = document.getElementById("checkout").value;
 
-    if (local && checkin && checkout) {
-        // Construir a URL do Trivago com a cidade digitada
-        const searchUrl = `https://www.trivago.com.br/?q=${encodeURIComponent(local)}&checkin=${encodeURIComponent(checkin)}&checkout=${encodeURIComponent(checkout)}`;
-        // Redirecionar para a URL
-        window.location.href = searchUrl;
-    } else {
-        alert("Por favor, preencha todos os campos.");
-    }
+  if (local && checkin && checkout) {
+    // Construir a URL do Trivago com a cidade digitada
+    const searchUrl = `https://www.trivago.com.br/?q=${encodeURIComponent(
+      local
+    )}&checkin=${encodeURIComponent(checkin)}&checkout=${encodeURIComponent(
+      checkout
+    )}`;
+    // Redirecionar para a URL
+    window.location.href = searchUrl;
+  } else {
+    alert("Por favor, preencha todos os campos.");
+  }
 });
 
+const backToTopButton = document.getElementById("backToTop");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
+
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
